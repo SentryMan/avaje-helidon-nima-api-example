c7d34747db67b3c3d86f68182b6e6c53e07f7609
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-02-23 00:45:41 -0500

    error advice

--- src/main/java/com/jojo/helidon/api/exception/ErrorAdvice.java
+++ src/main/java/com/jojo/helidon/api/exception/ErrorAdvice.java
@@ -1,5 +1,9 @@
 package com.jojo.helidon.api.exception;
 
+import java.io.OutputStream;
+
+import io.avaje.jsonb.JsonType;
+import io.avaje.jsonb.Jsonb;
 import io.helidon.common.http.Http.Status;
 import io.helidon.nima.webserver.http.HttpRouting.Builder;
 import io.helidon.nima.webserver.http.ServerRequest;
@@ -8,14 +12,26 @@ import jakarta.inject.Singleton;
 
 @Singleton
 public class ErrorAdvice {
-  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ErrorAdvice.class);
 
+  private final JsonType<ErrorResponse> errorType;
+
+  ErrorAdvice(Jsonb jsonb) {
+    this.errorType = jsonb.type(ErrorResponse.class);
+  }
+
+  // register the different error handling
   public Builder addErrorHandling(Builder builder) {
     builder.error(Exception.class, this::handle);
     return builder;
   }
 
   private void handle(ServerRequest req, ServerResponse res, Exception ex) {
-    res.status(Status.INTERNAL_SERVER_ERROR_500).send(ex.getMessage());
+    final var outputStream = getServletStream(Status.INTERNAL_SERVER_ERROR_500, res);
+    final var error = new ErrorResponse("5000", ex.getMessage());
+    errorType.toJson(error, outputStream);
+  }
+
+  OutputStream getServletStream(Status status, ServerResponse res) {
+    return res.status(status).header("Content-Type", "application/json").outputStream();
   }
 }
