811fdcf11526e04ba4e4e3f742e49165eadac403
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-06-09 13:53:36 -0400

    back

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 mvn clean package
-./target/jlink-image/bin/java -Dlogback.configurationFile=logback-local.xml -Dprofile=local -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 --enable-preview -m nima.example/com.jojo.helidon.api.NimaAPI
+./target/jlink-image/bin/java -Dlogback.configurationFile=logback.xml -Dprofile=local -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 --enable-preview -m nima.example/com.jojo.helidon.api.NimaAPI
