0a856e9eb829f1ec7f2345fa1fa7efa9c5e1e6f2
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-08 15:32:44 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,15 @@ module nima.example {
   requires io.avaje.inject;
   requires io.avaje.config;
   requires io.avaje.http.client;
-  requires io.avaje.jsonb;
+  requires io.avaje.jsonb.plugin;
   requires io.helidon.nima.webserver;
   requires io.avaje.http.api;
   requires org.slf4j;
 
   provides io.avaje.inject.spi.Module with
       com.jojo.helidon.api.ApiModule;
-  provides io.avaje.http.client.HttpApiProvider with
-      com.jojo.helidon.api.client.httpclient.ApiClientHttpClient.Provider;
+  provides io.avaje.http.client.HttpClient.GeneratedComponent with
+      com.jojo.helidon.api.client.httpclient.GeneratedHttpComponent;
   provides io.avaje.jsonb.Jsonb.GeneratedComponent with
       com.jojo.helidon.api.jsonb.GeneratedJsonComponent;
 }
