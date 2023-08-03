1bfdb01d9afe0ad042b432173a1b2450ad0e5a6f
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-02 20:45:10 -0400

    2.0

--- src/main/java/com/jojo/helidon/api/config/ServerFactory.java
+++ src/main/java/com/jojo/helidon/api/config/ServerFactory.java
@@ -4,20 +4,19 @@ import io.avaje.config.Config;
 import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
 import io.helidon.nima.webserver.WebServer;
+import io.helidon.nima.webserver.http.HttpFeature;
 import io.helidon.nima.webserver.http.HttpRouting;
-import io.helidon.nima.webserver.http.HttpService;
 import java.util.List;
 
 @Factory
 public class ServerFactory {
 
   @Bean
-  WebServer server(List<HttpService> routes) {
+  WebServer server(List<HttpFeature> routes) {
 
     final var builder = HttpRouting.builder();
-    for (final HttpService httpService : routes) {
-      httpService.routing(builder);
-    }
+
+    routes.forEach(builder::addFeature);
 
     return WebServer.builder()
         .addRouting(builder.build())
--- src/main/java/com/jojo/helidon/api/exception/ErrorAdvice.java
+++ src/main/java/com/jojo/helidon/api/exception/ErrorAdvice.java
@@ -1,41 +1,13 @@
 package com.jojo.helidon.api.exception;
 
-import io.avaje.jsonb.JsonType;
-import io.avaje.jsonb.Jsonb;
-import io.avaje.jsonb.stream.JsonOutput;
-import io.helidon.common.http.Http.Status;
-import io.helidon.nima.webserver.http.HttpRouting.Builder;
-import io.helidon.nima.webserver.http.HttpRules;
-import io.helidon.nima.webserver.http.HttpService;
-import io.helidon.nima.webserver.http.ServerRequest;
-import io.helidon.nima.webserver.http.ServerResponse;
-import jakarta.inject.Singleton;
+import io.avaje.http.api.Controller;
+import io.avaje.http.api.ExceptionHandler;
 
-@Singleton
-public class ErrorAdvice implements HttpService {
+@Controller
+public class ErrorAdvice {
 
-  private final JsonType<ErrorResponse> errorType;
-
-  ErrorAdvice(Jsonb jsonb) {
-    this.errorType = jsonb.type(ErrorResponse.class);
-  }
-
-  // register the different error handling
-  @Override
-  public void routing(HttpRules rules) {
-    if (rules instanceof final Builder builder) {
-      builder.error(Exception.class, this::handle);
-    }
-  }
-
-  private void handle(ServerRequest req, ServerResponse res, Exception ex) {
-    final var outputStream = getServletStream(Status.INTERNAL_SERVER_ERROR_500, res);
-    final var error = new ErrorResponse("5000", ex.getMessage());
-    errorType.toJson(error, outputStream);
-  }
-
-  JsonOutput getServletStream(Status status, ServerResponse res) {
-
-    return JsonOutput.of(res.status(status).header("Content-Type", "application/json"));
+  @ExceptionHandler
+  public ErrorResponse handle(Exception ex) {
+    return new ErrorResponse("5000", ex.getMessage());
   }
 }
