89d3d189e61316e400671a3273974654b636ab79
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-07-12 09:09:15 -0400

    update apm name

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=DELTA
 export OTEL_EXPERIMENTAL_EXPORTER_OTLP_RETRY_ENABLED=true
 export OTEL_EXPORTER_OTLP_METRICS_DEFAULT_HISTOGRAM_AGGREGATION=BASE2_EXPONENTIAL_BUCKET_HISTOGRAM
 export OTEL_EXPORTER_OTLP_COMPRESSION=gzip
-export OTEL_SERVICE_NAME=helidon-se-test
+export OTEL_SERVICE_NAME=helidon-apm-test
 export OTEL_EXPERIMENTAL_RESOURCE_DISABLED_KEYS=process.command_args
 mvn clean package
 java -javaagent:opentelemetry-javaagent.jar --module-path ./target/modules -Dconfig.profiles=local -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 --enable-preview -m nima.example/com.jojo.helidon.api.NimaAPI
