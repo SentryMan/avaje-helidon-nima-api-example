b543c3a650b64c5542c33d9f2e1d2a9e305b0508
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-10-20 22:55:29 -0400

    use provides plugin to modify modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,13 +7,4 @@ module nima.example {
   requires io.avaje.validation.contraints;
   requires io.avaje.validation.http;
   requires io.avaje.logback.encoder;
-
-  provides io.avaje.inject.spi.InjectExtension with
-      com.jojo.helidon.api.ApiModule;
-  provides io.avaje.http.client.HttpClient.GeneratedComponent with
-      com.jojo.helidon.api.client.httpclient.GeneratedHttpComponent;
-  provides io.avaje.jsonb.spi.JsonbExtension with
-      com.jojo.helidon.api.jsonb.GeneratedJsonComponent;
-  provides io.avaje.validation.spi.ValidationExtension with
-      com.jojo.helidon.api.controller.valid.GeneratedValidatorComponent;
 }
