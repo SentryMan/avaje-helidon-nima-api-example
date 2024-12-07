4d0e19ac906c23eb1f970bd9cf0d988357cdafb1
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-12-07 11:50:07 -0500

    module main class

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-java --module-path ./target/modules -Dconfig.profiles=local -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 --enable-preview -m nima.example/com.jojo.helidon.api.NimaAPI
+java --module-path ./target/modules -Dconfig.profiles=local --enable-preview -m nima.example
--- runJpackage.sh
+++ runJpackage.sh
@@ -1,10 +1,10 @@
 mvn clean package
 cd ./target 
-jpackage --type app-image --name avaje-helidon --module nima.example/com.jojo.helidon.api.NimaAPI \
+jpackage --type app-image --name avaje-helidon --module nima.example \
 --module-path ./modules  --add-modules nima.example 
 cd ..
 ## for windows
-## ./target/avaje-helidon/avaje-helidon
+ ./target/avaje-helidon/avaje-helidon
 ## for mac
 ## ./target/avaje-helidon.app/Contents/MacOS/avaje-helidon
-./target/avaje-helidon.app/Contents/MacOS/avaje-helidon
+## ./target/avaje-helidon.app/Contents/MacOS/avaje-helidon
