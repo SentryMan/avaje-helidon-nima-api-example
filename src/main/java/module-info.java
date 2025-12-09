b40683091c8be5c94581dd86bcd858ff26e7bd8c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-12-09 17:22:50 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module nima.example {
   requires jakarta.inject;
   requires io.avaje.inject.aop;
   requires io.avaje.http.client;
+  requires io.avaje.http.plugin;
   requires io.avaje.validation.contraints;
   requires io.avaje.validation.http;
   requires io.avaje.logback.encoder;
